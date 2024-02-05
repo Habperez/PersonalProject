0a19440098d89d69acee7cf551496166da9a035d
Habperez <114546644+Habperez@users.noreply.github.com>
2024-02-05 14:26:22 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+body {
+  text-align: center;
+  padding-top: 100px;
+  font-family: Arial, sans-serif;
+}
+
+button {
+  font-size: 20px;
+  padding: 10px 20px;
+  margin: 20px;
+  cursor: pointer;
+}
