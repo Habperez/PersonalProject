f3aa012380980790e94b79e83ab65c9de90e7451
Habperez <114546644+Habperez@users.noreply.github.com>
2024-02-05 16:32:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const question = document.getElementById('question');
   const loveMessage = document.getElementById('loveMessage');
   const loveImage = document.getElementById('loveImage');
+  const funnyGif = document.getElementById('Gif');
   let attemptCount = 0;
-  const maxAttempts = 15;
+  const maxNoAttempts = 15;
+  const maxGifAttempts = 30;
   
   function moveButton() {
     noBtn.style.position = 'absolute';
@@ -14,18 +16,26 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   noBtn.addEventListener('mouseover', function() {
-    moveButton();
-    attemptCount++;
-    if (attemptCount === maxAttempts) {
+    if (attemptCount < maxGifAttempts) {
+      attemptCount++;
+      moveButton();
+    }
+
+    if (attemptCount === maxNoAttempts) {
       noBtn.classList.add('no-image-btn');
-      
       noBtn.innerHTML = '';
       const img = document.createElement('img');
       img.src = 'fern.jpg';
       img.alt = 'No';
-      img.style.width = '100px';
+      img.style.width = '200px';
       img.style.height = 'auto';
       noBtn.appendChild(img);
+    }
+
+
+    if (attemptCount === maxGifAttempts) {
+      funnyGif.classList.remove('hidden');
+      funnyGif.classList.add('unhidden');
     }
   });
 
@@ -34,6 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
     noBtn.style.display = 'none';
     question.textContent = 'I love you';
 
+    
     loveImage.src = 'Peanut.png'; 
     loveMessage.classList.remove('hidden');
     loveMessage.classList.add('unhidden');
