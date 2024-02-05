e583cfe25231d0729bd1023dfc37747e636a0f95
Habperez <114546644+Habperez@users.noreply.github.com>
2024-02-05 14:48:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
   const noBtn = document.getElementById('noBtn');
   const yesBtn = document.getElementById('yesBtn');
+  const heading = document.querySelector('h2');
   const loveMessage = document.getElementById('loveMessage');
   const loveImage = document.getElementById('loveImage');
 
-  // "No"
   noBtn.addEventListener('mouseover', function() {
-
     noBtn.style.position = 'absolute';
     noBtn.style.left = Math.random() * (window.innerWidth - noBtn.clientWidth) + 'px';
     noBtn.style.top = Math.random() * (window.innerHeight - noBtn.clientHeight) + 'px';
   });
 
   yesBtn.addEventListener('click', function() {
-
+    // Hide the buttons
     yesBtn.style.display = 'none';
     noBtn.style.display = 'none';
+
+    heading.textContent = 'I love you';
 
     loveImage.src = 'Peanut.png';
     loveMessage.classList.remove('hidden');
